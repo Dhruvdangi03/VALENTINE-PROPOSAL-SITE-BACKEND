e7eb42fab9a592494c58b62f9b938599f6e4c385
dhruvdangi03 <dhruvsingdangi03@gmail.com>
2026-02-13 21:07:29 +0530

    added database logic

--- internals/config/database.go
+++ internals/config/database.go
@@ -1,1 +1,32 @@
 package config
+
+import (
+	"log"
+	"os"
+
+	"Valentine-Proposal-Site-Backend/internals/models"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDatabase() {
+	_ = godotenv.Load()
+
+	dsn := os.Getenv("DATABASE_URL")
+	if dsn == "" {
+		log.Fatal("❌ DATABASE_URL not set")
+	}
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("❌ Failed to connect to database:", err)
+	}
+
+	DB = db
+	db.AutoMigrate(&models.Proposal{})
+	log.Println("✅ PostgreSQL connected with GORM")
+}
